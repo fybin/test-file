189ae2618dba64161cf9481a4c1340454e6efb7f
fanyubin-mac <fanyubin-mac@fanyubin-macdeMacBook-Air.local>
2019-02-01 17:34:09 +0800

    更新带参数的装饰器

--- test1/t8.py
+++ test1/t8.py
@@ -1,6 +1,9 @@
 import logging
 
-
+# 带参数的装饰器
+# 带参数的装饰器只需要在原来那个不带参数的装饰器基础上之上在最外层套一个函数，
+# 该函数中定义一个参数，然后嵌套函数中引用该参数即可实现。从下图看出，我只是把里面那个函数改了一下名字，
+# 其余和原来不带参数的装饰器是一样的。
 def use_logging(level):
     def decorator(func):
         def wrapper(*args, **kwargs):
